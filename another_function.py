0f72c41d714377102a1b802dcdad119ae76b6b41
Carvin Chen <44011657+bluebluecard@users.noreply.github.com>
2019-08-05 22:13:42 +0800

    Update another_function.py

--- another_function.py
+++ another_function.py
@@ -6,3 +6,30 @@ def get_overlap_region(a,b):
 			else None
 	
 print(get_overlap_region([5,20],[3,20]))	
+
+#求极小值
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+step=0.03
+t0=0.01
+
+X=[]
+Y=[]
+for o in range(1,1000000):
+	x=t0
+	a=1-math.exp(-1*x)
+	b=math.pow(a,x)
+	c=math.log(a)
+	d=math.exp(-1*x)*x
+	y=b*(c+d/a)
+	t=t0-step*y
+	X.append(o)
+	Y.append(t0)
+	if abs(t - t0) < 0.0000000001:
+		break
+	
+	t0=t
+plt.plot(X,Y)
+plt.show()	
