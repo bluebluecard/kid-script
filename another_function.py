deb49085dbaf8f763404fa18d6c75abd4c325629
Carvin Chen <44011657+bluebluecard@users.noreply.github.com>
2020-04-20 14:29:38 +0800

    Update another_function.py

--- another_function.py
+++ another_function.py
@@ -33,3 +33,34 @@ for o in range(1,1000000):
 	t0=t
 plt.plot(X,Y)
 plt.show()	
+
+
+#merge_overlap_region,return length of merge region
+def run_pick(last_cor,now_cor):
+    if last_cor[1] >= now_cor[1]:
+        merge_cor = last_cor
+    else:
+        merge_cor = [last_cor[0],now_cor[1]]
+    return merge_cor
+
+def merge(a):
+    data = []
+    a = sorted(a,key=lambda x:x[0])
+    last_cor = a[0]
+    total_length = 0
+    for i in range(len(a)):
+        if last_cor[0]<= a[i][0] <= last_cor[1]:
+            merge_cor = run_pick(last_cor,a[i])
+            last_cor = merge_cor
+        else:
+            data.append(last_cor)
+            last_cor = a[i]
+        if i == len(a) - 1:
+            data.append(last_cor)
+    for region in data:
+        total_length += region[1]-region[0]
+    return total_length
+
+
+
+print(merge([[1,10],[5,9],[9,18],[20,30]]))
